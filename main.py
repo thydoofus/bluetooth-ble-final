3ede3bcba188135e20658537643611f7ebf3cd8a
thydoofus <77323809+thydoofus@users.noreply.github.com>
2022-11-03 18:56:06 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ def on_button_pressed_a():
     keyboard.send_string("v")
     control.wait_micros(50)
     basic.show_icon(IconNames.HEART)
+
+def on():
+    basic.show_icon(IconNames.YES)
+    keyboard.send_string("v")
+    control.wait_micros(50)
+    basic.show_icon(IconNames.HEART)
+
+def off():
+    basic.show_icon(IconNames.YES)
+    keyboard.send_string("v")
+    control.wait_micros(50)
+    basic.show_icon(IconNames.HEART)
+
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 keyboard.start_keyboard_service()
@@ -10,11 +23,8 @@ basic.show_icon(IconNames.HEART)
 
 def on_forever():
     if pins.pulse_in(DigitalPin.P0, PulseValue.HIGH) >= 500:
-        basic.show_icon(IconNames.HAPPY)
-        keyboard.send_string("v")
-        if pins.analog_read_pin(AnalogPin.P0) < 700:
-            basic.show_icon(IconNames.SAD)
-            keyboard.send_string("x")
+        on_button_pressed_a()
+        if pins.pulse_in(DigitalPin.P0, PulseValue.LOW) >= 500:
+            on_button_pressed_a()
             return
-    return
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,22 +1,30 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+function on_button_pressed_a () {
     basic.showIcon(IconNames.Yes)
     keyboard.sendString("v")
     control.waitMicros(50)
     basic.showIcon(IconNames.Heart)
-})
+}
+function off () {
+    basic.showIcon(IconNames.Yes)
+    keyboard.sendString("v")
+    control.waitMicros(50)
+    basic.showIcon(IconNames.Heart)
+}
+function on () {
+    basic.showIcon(IconNames.Yes)
+    keyboard.sendString("v")
+    control.waitMicros(50)
+    basic.showIcon(IconNames.Heart)
+}
+input.onButtonPressed(Button.A, on_button_pressed_a)
 keyboard.startKeyboardService()
 basic.showIcon(IconNames.Heart)
-basic.forever(function on_forever() {
+basic.forever(function () {
     if (pins.pulseIn(DigitalPin.P0, PulseValue.High) >= 500) {
-        basic.showIcon(IconNames.Happy)
-        keyboard.sendString("v")
-        if (pins.analogReadPin(AnalogPin.P0) < 700) {
-            basic.showIcon(IconNames.Sad)
-            keyboard.sendString("x")
+        on_button_pressed_a()
+        if (pins.pulseIn(DigitalPin.P0, PulseValue.Low) >= 500) {
+            on_button_pressed_a()
             return
         }
-        
     }
-    
-    return
 })
