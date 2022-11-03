67a715a04dbc91efc8493890756ebd8cf7d75278
thydoofus <77323809+thydoofus@users.noreply.github.com>
2022-11-03 21:07:58 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,24 +3,26 @@ def on_button_pressed_a():
     keyboard.send_string("v")
     control.wait_micros(50)
     basic.show_icon(IconNames.HEART)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def off():
     basic.show_icon(IconNames.SAD)
     keyboard.send_string("v")
-    control.wait_micros(50)
+    control.wait_micros(100)
     basic.show_icon(IconNames.HEART)
 def on():
     basic.show_icon(IconNames.HAPPY)
-    keyboard.send_string("v")
-    control.wait_micros(50)
+    keyboard.send_string("x")
+    control.wait_micros(100)
     basic.show_icon(IconNames.HEART)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_pin_released_p1():
+    off()
+input.on_pin_released(TouchPin.P1, on_pin_released_p1)
 keyboard.start_keyboard_service()
 basic.show_icon(IconNames.HEART)
 
 def on_forever():
-    if pins.pulse_in(DigitalPin.P0, PulseValue.HIGH) >= 500:
+    if input.pin_is_pressed(TouchPin.P0):
         on()
-        if pins.pulse_in(DigitalPin.P0, PulseValue.LOW) >= 500:
-            off()
-            return
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,33 +1,28 @@
-function off() {
-    basic.showIcon(IconNames.Sad)
-    keyboard.sendString("v")
-    control.waitMicros(50)
-    basic.showIcon(IconNames.Heart)
-}
-
-function on() {
-    basic.showIcon(IconNames.Happy)
-    keyboard.sendString("v")
-    control.waitMicros(50)
-    basic.showIcon(IconNames.Heart)
-}
-
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
     keyboard.sendString("v")
     control.waitMicros(50)
     basic.showIcon(IconNames.Heart)
 })
+function off () {
+    basic.showIcon(IconNames.Sad)
+    keyboard.sendString("v")
+    control.waitMicros(100)
+    basic.showIcon(IconNames.Heart)
+}
+function on () {
+    basic.showIcon(IconNames.Happy)
+    keyboard.sendString("x")
+    control.waitMicros(100)
+    basic.showIcon(IconNames.Heart)
+}
+input.onPinReleased(TouchPin.P1, function () {
+    off()
+})
 keyboard.startKeyboardService()
 basic.showIcon(IconNames.Heart)
-basic.forever(function on_forever() {
-    if (pins.pulseIn(DigitalPin.P0, PulseValue.High) >= 500) {
+basic.forever(function () {
+    if (pins.analogReadPin(AnalogPin.P1) >= 1000) {
         on()
-        if (pins.pulseIn(DigitalPin.P0, PulseValue.Low) >= 500) {
-            off()
-            return
-        }
-        
     }
-    
 })
