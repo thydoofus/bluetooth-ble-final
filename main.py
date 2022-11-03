0ebb1a9faf8f54534781a53da7d7f58a17de0d1a
thydoofus <77323809+thydoofus@users.noreply.github.com>
2022-11-03 18:57:10 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,28 +3,24 @@ def on_button_pressed_a():
     keyboard.send_string("v")
     control.wait_micros(50)
     basic.show_icon(IconNames.HEART)
-
-def on():
-    basic.show_icon(IconNames.YES)
-    keyboard.send_string("v")
-    control.wait_micros(50)
-    basic.show_icon(IconNames.HEART)
-
 def off():
-    basic.show_icon(IconNames.YES)
+    basic.show_icon(IconNames.SAD)
     keyboard.send_string("v")
     control.wait_micros(50)
     basic.show_icon(IconNames.HEART)
-
+def on():
+    basic.show_icon(IconNames.HAPPY)
+    keyboard.send_string("v")
+    control.wait_micros(50)
+    basic.show_icon(IconNames.HEART)
 input.on_button_pressed(Button.A, on_button_pressed_a)
-
 keyboard.start_keyboard_service()
 basic.show_icon(IconNames.HEART)
 
 def on_forever():
     if pins.pulse_in(DigitalPin.P0, PulseValue.HIGH) >= 500:
-        on_button_pressed_a()
+        on()
         if pins.pulse_in(DigitalPin.P0, PulseValue.LOW) >= 500:
-            on_button_pressed_a()
+            off()
             return
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,30 +1,33 @@
-function on_button_pressed_a () {
-    basic.showIcon(IconNames.Yes)
+function off() {
+    basic.showIcon(IconNames.Sad)
     keyboard.sendString("v")
     control.waitMicros(50)
     basic.showIcon(IconNames.Heart)
 }
-function off () {
-    basic.showIcon(IconNames.Yes)
+
+function on() {
+    basic.showIcon(IconNames.Happy)
     keyboard.sendString("v")
     control.waitMicros(50)
     basic.showIcon(IconNames.Heart)
 }
-function on () {
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showIcon(IconNames.Yes)
     keyboard.sendString("v")
     control.waitMicros(50)
     basic.showIcon(IconNames.Heart)
-}
-input.onButtonPressed(Button.A, on_button_pressed_a)
+})
 keyboard.startKeyboardService()
 basic.showIcon(IconNames.Heart)
-basic.forever(function () {
+basic.forever(function on_forever() {
     if (pins.pulseIn(DigitalPin.P0, PulseValue.High) >= 500) {
-        on_button_pressed_a()
+        on()
         if (pins.pulseIn(DigitalPin.P0, PulseValue.Low) >= 500) {
-            on_button_pressed_a()
+            off()
             return
         }
+        
     }
+    
 })
